708f32c37dbcbbe101faecac0df9ab32aa3b19ee
Blinter <90986012+Blinter@users.noreply.github.com>
2024-10-18 22:43:15 -0400

    fix: router paths, 404

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://blinter.github.io/React-Router-ColorFactory/'
+  base: '/React-Router-ColorFactory/',
+  server: {
+    middlewareMode: 'html'
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        index: 'index.html',
+        '404': '404.html'
+      }
+    }
+  }
 })
